ca57bbdeefbb188813d1ba988f8d00e76eb8ee69
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-05 21:23:04 -0400

    completed two test

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const exampleProducts = [
 */
 function getCartTotal(cart) {
   let result = 0;
+  if(cart.length === 0){
+    throw `Cart is empty`
+  }
   for (let product of cart) {
     result += product.priceInCents;
   }
@@ -37,7 +40,25 @@ function getCartTotal(cart) {
 */
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
+  if(products.length === 0){
+    throw `Not available`
+  }
+  if((min !== Number(min)|| max !== Number(max))){
+    throw `Not a number`
+  }
+  if(max === 0){
+    throw `Max is zero`
+  }
+  if(min > max){
+    throw `min is greater than max`
+  }
+  if((min || max) < 0){
+    throw `negative numbers`
+  }
   for (let product of products) {
+    if(!product.priceInCents){
+      throw `product does not have priceInCents`
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +70,19 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+
   const filteredProducts = filterProductsByPriceRange(products, min, max);
   const total = getCartTotal(filteredProducts);
-
   return total;
+  console.log(filteredProducts)
+try{
+
+
+}catch(error){
+  throw `0`
+}
+
+
 }
 
 module.exports = {
