d0f2f0e3d8e89df03de8599a9eb7644559756bfd
Kenneth-Parker <kennethkavon@gmail.com>
2023-06-12 06:24:30 -0400

    tests threw good ✅

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ const exampleProducts = [
   This function should throw an error if:
   - The `cart` array is empty.
 */
-function getCartTotal(cart) {
+const getCartTotal = (cart) => {
   let result = 0;
+  if (cart.length === 0) throw `The 'cart' array is empty.`
   for (let product of cart) {
     result += product.priceInCents;
   }
@@ -35,7 +36,16 @@ function getCartTotal(cart) {
   - Either `min` or `max` is less than `0`.
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
-function filterProductsByPriceRange(products, min, max) {
+const filterProductsByPriceRange = (products, min, max) => {
+  if (products.length === 0) throw "The 'products' array is empty."
+  if (typeof min !== "number" || typeof max !== "number") throw "Either `min` or `max` is not a number."
+  if (max === 0) throw "`max` is equal to `0`."
+  if (min > max) throw "`min` is greater than `max`."
+  if ((min || max) < 0) throw "Either `min` or `max` is less than `0`."
+  for (const product of products) {
+    if (!product.priceInCents) throw "Any of the products in the `products` array does not have a `priceInCents` key."
+  }
+
   const result = [];
   for (let product of products) {
     if (product.priceInCents >= min && product.priceInCents <= max) {
@@ -48,11 +58,14 @@ function filterProductsByPriceRange(products, min, max) {
 /*
   If any errors occur in this function, it should return `0`.
 */
-function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+const getTotalOfAllProductsByPriceRange = (products, min, max) => {
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) { 
+    return 0 
+  }
 }
 
 module.exports = {
